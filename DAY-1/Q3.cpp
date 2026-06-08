294e31282d9d269cf67a329036256ee01318b36e
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-06-09 00:12:20 +0530

    DAY-1 Q1 completed

--- DAY-1/Q3.cpp
+++ DAY-1/Q3.cpp
@@ -1,1 +1,13 @@
-
+//Write a program to Find factorial of a number. 
+#include <iostream>
+using namespace std;
+int main(){
+    int i,fact=1,n;
+    cout << "Enter Number : ";
+    cin >> n;
+    for(i=n;i>1;i--){
+        fact=fact*i;
+    }
+    cout << "Factorial of Number " << n << " = " << fact;
+    return 0;
+}
---
+++ DAY-1/Q4.cpp
@@ -0,0 +1,15 @@
+//Write a program to Count digits in a number.
+#include <iostream>
+using namespace std;
+int main(){
+    int temp=0,count=0,n;
+    cout << "Enter Any Number : ";
+    cin >> n;
+    temp=n;
+    while(temp!=0){
+        temp=temp/10;
+        count=count+1;
+    }
+    cout << "Number of Digits in " << n << " is = " << count;
+    return 0;
+}
