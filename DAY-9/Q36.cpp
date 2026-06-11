4860f44f414cb29cfcced1b660aa6f13c996a68d
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-06-11 20:35:59 +0530

    DAY-9 Uploaded

---
+++ DAY-9/Q36.cpp
@@ -0,0 +1,25 @@
+/*  Write a program to Print hollow square
+    pattern.
+    *****
+    *   *
+    *   *
+    *   *
+    *****
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,n;
+    cout << "enter no of rows = ";
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            if (i == 1 || i == n || j == 1 || j == n) {
+                cout << "*"; } 
+                else {
+                cout << " "; }  }
+        cout << "\n";
+    }
+    return 0; }
