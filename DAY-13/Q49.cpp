05f1e1f1317460ca6b5134b19d8838eb68d55586
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-06-14 16:00:44 +0530

    DAY-13 Uploaded

---
+++ DAY-13/Q49.cpp
@@ -0,0 +1,18 @@
+//Write a program to Input and display array.
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout << "enter number of elements in array : ";
+    cin >> n;
+    int a[n];
+    for(i=1;i<=n;i++){
+        cout << "enter element " << i << " : ";
+        cin >> a[i];
+    }
+    cout << "elements of array are : ";
+    for(i=1;i<=n;i++){
+        cout << a[i] << " ";
+    }
+    return 0;
+}
---
+++ DAY-13/Q50.cpp
@@ -0,0 +1,21 @@
+//Write a program to Find sum and average of array.
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i;
+    double avg=0,sum=0;
+    cout << "enter number of elements of array : ";
+    cin >> n;
+    int a[n];
+    for(i=0;i<n;i++){ 
+        cout << "enter element " << i+1 << " : ";
+        cin >> a[i];
+    }
+    for(i=0;i<n;i++){
+        sum=sum+a[i];
+    }
+    avg=sum/n;
+    cout << "sum of the array : " << sum << "\n";
+    cout << "average of the array : " << avg << "\n";
+    return 0;
+}
