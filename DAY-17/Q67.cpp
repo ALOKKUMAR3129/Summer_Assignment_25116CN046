56db14187ca2ea0a9f86373029b729e06cca4ef1
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-06-17 21:20:56 +0530

    DAY-18 Uploaded

---
+++ DAY-17/Q67.cpp
@@ -0,0 +1,36 @@
+//Write a program to Intersection of arrays.
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,k,n;
+    cout << "enter nof of elements of the array : ";
+    cin >> n;
+    int a[n];
+    int b[n];
+    int in[50]={0};
+    int totalElements = 0;
+    cout << "enter first array elements" << "\n";
+    for(i=0;i<n;i++){
+        cout << "enter element " << i+1 << " : ";
+        cin >> a[i];
+    }
+    cout << "enter second array elements" << "\n";
+    for(i=0;i<n;i++){
+        cout << "enter element " << i+1 << " : ";
+        cin >> b[i];
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(a[i]==b[j]){
+            in[totalElements]=a[i];
+            totalElements++;
+            break;
+            }
+        }
+    }
+    cout << "intersection array : ";
+    for(i=0;i<totalElements;i++){
+        cout << in[i] << " ";
+    }
+    return 0;
+}
