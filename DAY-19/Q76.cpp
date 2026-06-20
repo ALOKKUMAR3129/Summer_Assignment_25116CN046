d7ab8b061c807629d01724d5f8d5eae0b19262f5
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-06-20 23:50:45 +0530

    DAY-19 Uploaded

---
+++ DAY-19/Q76.cpp
@@ -0,0 +1,23 @@
+//Write a program to Find diagonal sum.
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,k,size,sum=0;
+    cout << "enter size of square matrix (rows/columns) = ";
+    cin >> size;
+    int a[size][size];
+    
+    cout << "\n--- Enter Matrix Elements ---\n";
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            cout << "Enter element at row " << i+1 << ", col " << j+1 << ": ";
+            cin >> a[i][j];
+        }
+    }
+    for(i=0;i<size;i++){
+        sum = sum + a[i][i];
+    }
+    
+    cout << "\nSum of primary diagonal elements : " << sum << "\n";
+    return 0;
+}
